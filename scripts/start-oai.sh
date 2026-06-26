9fe33d8a5cfbe92fdc5404c0e5738f79ba9f9987
gnbHPLaptop <alexandreserio@ua.pt>
2026-06-26 15:43:26 +0100

    Added user detection command in "start-oai.sh"

--- scripts/start-oai.sh
+++ scripts/start-oai.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+USER=$(users | awk '{print $1}')
 
 echo -e "\n\033[0;34m[Systemd Service]\033[0m Starting OAI\033[0m\n"
 
-cd /home/"user"/openairinterface5g/cmake_targets/ran_build/build
+cd /home/$USER/oai/Scripts/
 if [ "$1" = "ue" ]; then 
     # Add | ts "%Y-%m-%d %H:%M:%S" at the end of the script for timestamps on log file
-    taskset -c 2,3 ./nr-uesoftmodem -C 765500000 --CO -55000000 -r 25 --numerology 0 --band 28 --ssb 53 --rfsim | ts "%Y-%m-%d %H:%M:%S"  # --log_config.global_log_level "debug" 
+    #taskset -c 1,2,3 ./nr-uesoftmodem -O ../../../../conf_files/nrUEs/nrue_b205_rooftop.conf | ts "%Y-%m-%d %H:%M:%S"  # --log_config.global_log_level "debug" 
+    ./ue_deployment.sh -5floor | ts "%Y-%m-%d %H:%M:%S"
 elif [ "$1" = "gnb" ]; then
-    ./nr-softmodem -O ../../../../oai/conf_files/gnb.sa.band28.fr1.25PRB.rfsim.conf --rfsim
+    ./gNB_deployment.sh -5floor | ts "%Y-%m-%d %H:%M:%S"
 fi
 
 /usr/local/bin/oai-stats.sh systemd_restart
